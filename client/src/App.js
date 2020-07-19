c51d8419a71fdd10a9afcabddaed8060f8d933c8
sgneha <sgneha05@gmail.com>
2020-07-20 00:01:55 +0100

    adding Router and Route in App

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layouts/Navbar";
 import Landing from "./components/layouts/Landing";
 
 import "./App.css";
 
 const App = () => (
-  <Fragment>
-    <Navbar />
-    <Landing />
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path="/" component={Landing} />
+    </Fragment>
+  </Router>
 );
 
 export default App;
