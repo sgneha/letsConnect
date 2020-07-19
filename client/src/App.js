6a96d0c060e0eb21675c133a7da7f0f036a95c7f
sgneha <sgneha05@gmail.com>
2020-07-19 23:47:34 +0100

    Landing & Navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,14 @@
 import React, { Fragment } from "react";
+import Navbar from "./components/layouts/Navbar";
+import Landing from "./components/layouts/Landing";
 
 import "./App.css";
 
-const App = () => <Fragment>App</Fragment>;
+const App = () => (
+  <Fragment>
+    <Navbar />
+    <Landing />
+  </Fragment>
+);
 
 export default App;
